59f4b001f2e6dcac38f9ab8f03156cd4d6b1dbfb
aaronUnosquare <88786786+aaronUnosquare@users.noreply.github.com>
2022-02-10 01:35:31 -0600

    Set a route location to each button from menu in desktop

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -1,20 +1,21 @@
 import { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Link as RouterLink } from 'react-router-dom';
 import {
-  AppBar,
-  Button,
-  IconButton,
-  Toolbar,
-  Typography,
-  withStyles,
-  Menu,
-  MenuItem,
+	AppBar,
+	Button,
+	IconButton,
+	Toolbar,
+	Typography,
+	withStyles,
+	Menu,
+	MenuItem,
 } from '@material-ui/core';
 import {
-  Translate,
-  KeyboardArrowDown,
-  Menu as MenuIcon,
+	Translate,
+	KeyboardArrowDown,
+	Menu as MenuIcon,
 } from '@material-ui/icons';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
@@ -29,122 +30,124 @@ import styles from './styles';
 const useStyles = makeStyles(rootStyle);
 
 const MenuBar = ({
-  classes,
-  languageMenuItemSelected,
-  languageLabel,
-  setMenuItemSelected,
-  setLanguageLabel,
-  openDrawer,
+	classes,
+	languageMenuItemSelected,
+	languageLabel,
+	setMenuItemSelected,
+	setLanguageLabel,
+	openDrawer,
 }) => {
-  const [t, i18n] = useTranslation();
-  const [anchorEl, setAnchorEl] = useState(null);
-  const rootClasses = useStyles();
-  const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up('laptop'));
-  const showMenuIcon = useMediaQuery(theme.breakpoints.up('laptop'));
+	const [t, i18n] = useTranslation();
+	const [anchorEl, setAnchorEl] = useState(null);
+	const rootClasses = useStyles();
+	const theme = useTheme();
+	const matches = useMediaQuery(theme.breakpoints.up('laptop'));
+	const showMenuIcon = useMediaQuery(theme.breakpoints.up('laptop'));
 
-  const openChangeLanguage = (event) => setAnchorEl(event.currentTarget);
+	const openChangeLanguage = (event) => setAnchorEl(event.currentTarget);
 
-  const closeChangeLanguage = () => setAnchorEl(null);
+	const closeChangeLanguage = () => setAnchorEl(null);
 
-  const changeLanguage = (code, index, label) => {
-    i18n.changeLanguage(code);
-    setMenuItemSelected(index);
-    setLanguageLabel(label);
-    closeChangeLanguage();
-  };
+	const changeLanguage = (code, index, label) => {
+		i18n.changeLanguage(code);
+		setMenuItemSelected(index);
+		setLanguageLabel(label);
+		closeChangeLanguage();
+	};
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="sticky">
-        <Toolbar classes={{ root: rootClasses.container }}>
-          {!showMenuIcon && (
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-              onClick={openDrawer}
-            >
-              <MenuIcon />
-            </IconButton>
-          )}
-          <Typography variant="h6" className={classes.title}>
-            Aarón López Sosa
-          </Typography>
-          {/* show the menu options when the window increases */}
-          {matches &&
-            MenuOptions.map((item, index) => (
-              <Button
-                key={index}
-                className={classes.buttonOption}
-                color="inherit"
-              >
-                {t(item.label)}
-              </Button>
-            ))}
-          <Button
-            aria-controls="language-menu"
-            aria-haspopup="true"
-            color="inherit"
-            startIcon={<Translate />}
-            endIcon={<KeyboardArrowDown />}
-            onClick={openChangeLanguage}
-          >
-            {matches && t(languageLabel)}
-          </Button>
-          <Menu
-            id="language-menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={closeChangeLanguage}
-          >
-            {AVAILABLE_LANGUAGES.map((item, index) => (
-              <MenuItem
-                key={index}
-                selected={index === languageMenuItemSelected}
-                onClick={() => changeLanguage(item.code, index, item.label)}
-              >
-                {t(item.label)}
-              </MenuItem>
-            ))}
-          </Menu>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+	return (
+		<div className={classes.root}>
+			<AppBar position="sticky">
+				<Toolbar classes={{ root: rootClasses.container }}>
+					{!showMenuIcon && (
+						<IconButton
+							edge="start"
+							className={classes.menuButton}
+							color="inherit"
+							aria-label="menu"
+							onClick={openDrawer}
+						>
+							<MenuIcon />
+						</IconButton>
+					)}
+					<Typography variant="h6" className={classes.title}>
+						Aarón López Sosa
+					</Typography>
+					{/* show the menu options when the window increases */}
+					{matches &&
+						MenuOptions.map((item, index) => (
+							<Button
+								key={index}
+								className={classes.buttonOption}
+								color="inherit"
+								component={RouterLink}
+								to={item.to}
+							>
+								{t(item.label)}
+							</Button>
+						))}
+					<Button
+						aria-controls="language-menu"
+						aria-haspopup="true"
+						color="inherit"
+						startIcon={<Translate />}
+						endIcon={<KeyboardArrowDown />}
+						onClick={openChangeLanguage}
+					>
+						{matches && t(languageLabel)}
+					</Button>
+					<Menu
+						id="language-menu"
+						anchorEl={anchorEl}
+						keepMounted
+						open={Boolean(anchorEl)}
+						onClose={closeChangeLanguage}
+					>
+						{AVAILABLE_LANGUAGES.map((item, index) => (
+							<MenuItem
+								key={index}
+								selected={index === languageMenuItemSelected}
+								onClick={() => changeLanguage(item.code, index, item.label)}
+							>
+								{t(item.label)}
+							</MenuItem>
+						))}
+					</Menu>
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
 };
 
 MenuBar.propTypes = {
-  classes: PropTypes.shape({
-    root: PropTypes.string,
-    menuButton: PropTypes.string,
-    title: PropTypes.string,
-    buttonOption: PropTypes.string.isRequired,
-  }).isRequired,
-  languageMenuItemSelected: PropTypes.number.isRequired,
-  languageLabel: PropTypes.string.isRequired,
-  setMenuItemSelected: PropTypes.func.isRequired,
-  setLanguageLabel: PropTypes.func.isRequired,
-  openDrawer: PropTypes.func.isRequired,
+	classes: PropTypes.shape({
+		root: PropTypes.string,
+		menuButton: PropTypes.string,
+		title: PropTypes.string,
+		buttonOption: PropTypes.string.isRequired,
+	}).isRequired,
+	languageMenuItemSelected: PropTypes.number.isRequired,
+	languageLabel: PropTypes.string.isRequired,
+	setMenuItemSelected: PropTypes.func.isRequired,
+	setLanguageLabel: PropTypes.func.isRequired,
+	openDrawer: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = ({
-  system: { languageMenuItemSelected, languageLabel },
+	system: { languageMenuItemSelected, languageLabel },
 }) => ({
-  languageMenuItemSelected,
-  languageLabel,
+	languageMenuItemSelected,
+	languageLabel,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  setMenuItemSelected: (index) =>
-    dispatch(SYSTEM_ACTIONS.setLanguageMenuItemSelected(index)),
-  setLanguageLabel: (label) => dispatch(SYSTEM_ACTIONS.setLanguageLabel(label)),
-  openDrawer: () => dispatch(SYSTEM_ACTIONS.setDrawerOpen(true)),
+	setMenuItemSelected: (index) =>
+		dispatch(SYSTEM_ACTIONS.setLanguageMenuItemSelected(index)),
+	setLanguageLabel: (label) => dispatch(SYSTEM_ACTIONS.setLanguageLabel(label)),
+	openDrawer: () => dispatch(SYSTEM_ACTIONS.setDrawerOpen(true)),
 });
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
+	mapStateToProps,
+	mapDispatchToProps
 )(withStyles(styles)(MenuBar));
--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -3,29 +3,30 @@ import { MenuBar, DrawerMenu } from '../../components';
 import { ROUTES } from '../../utils/constants';
 
 const Layout = () => {
-  return (
-    <>
-      <MenuBar />
-      <Router>
-        <DrawerMenu />
-        <Switch>
-          {ROUTES.map((item, index) => (
-            <Route
-              key={index}
-              path={item.path}
-              component={item.component}
-              exact={item.exact}
-              strict={item.strict}
-            />
-          ))}
-          <Route path="*">
-            {/* TODO: Create a component that will render the not found page  */}
-            <h1>No Match</h1>
-          </Route>
-        </Switch>
-      </Router>
-    </>
-  );
+	return (
+		<>
+			{/* <MenuBar /> */}
+			<Router>
+				<MenuBar />
+				<DrawerMenu />
+				<Switch>
+					{ROUTES.map((item, index) => (
+						<Route
+							key={index}
+							path={item.path}
+							component={item.component}
+							exact={item.exact}
+							strict={item.strict}
+						/>
+					))}
+					<Route path="*">
+						{/* TODO: Create a component that will render the not found page  */}
+						<h1>No Match</h1>
+					</Route>
+				</Switch>
+			</Router>
+		</>
+	);
 };
 
 export default Layout;
