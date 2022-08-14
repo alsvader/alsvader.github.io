82b92f79dac92287c8aad922bf516b7a3894bec9
aaronUnosquare <aaron.sosa@unosquare.com>
2022-08-14 01:44:57 -0500

    Remove Suspense

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -4,16 +4,19 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    allowMultiLoading: false,
-    fallbackLng: 'en',
-    debug: true,
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+	.use(Backend)
+	.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		allowMultiLoading: false,
+		fallbackLng: 'en',
+		debug: true,
+		interpolation: {
+			escapeValue: false,
+		},
+		react: {
+			useSuspense: false,
+		},
+	});
 
 export default i18n;
