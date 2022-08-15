4faf78fa61bad5efa96edadf248b8d18681a7d19
aaronUnosquare <aaron.sosa@unosquare.com>
2022-08-14 21:50:46 -0500

    Fix the MUI style error

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import { ThemeProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import {
+	StylesProvider,
+	createGenerateClassName,
+} from '@material-ui/core/styles';
 import { Provider } from 'react-redux';
 import store from './redux';
 import theme from './utils/theme';
 import { Layout } from './pages';
 import './index.css';
 
+const generateClassName = createGenerateClassName({
+	productionPrefix: navigator.userAgent === 'ReactSnap' ? 'snap' : 'jss',
+});
+
 function App() {
 	return (
 		<Provider store={store}>
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-				<Layout />
-			</ThemeProvider>
+			<StylesProvider generateClassName={generateClassName}>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Layout />
+				</ThemeProvider>
+			</StylesProvider>
 		</Provider>
 	);
 }
